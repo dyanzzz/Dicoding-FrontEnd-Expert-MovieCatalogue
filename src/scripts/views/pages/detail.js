1b3f93e1fa69deb70658e200fb1bd36a71e24d6c
dian-tri-budianto <dian.budianto@berca.co.id>
2021-04-18 14:03:11 +0700

    implementation request from API using TheMovieDB

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,13 +1,26 @@
+import UrlParser from '../../routes/url-parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+ 
 const Detail = {
 	async render() {
 		return `
-			<h2>Detail page</h2>
+			<div id="movie" class="movie"></div>
 		`;
 	},
-
+ 
 	async afterRender() {
-		// fungsi ini akan dipanggil setelah di render
+		const url = UrlParser.parseActiveUrlWithoutCombiner();
+		console.log("#### "+JSON.stringify(url));
+		
+		const movie = await TheMovieDbSource.detailMovie(url.id);
+		const movieContainer = document.querySelector('#movie');
+
+		console.log(movie);
+		console.log(movieContainer);
+
+		movieContainer.innerHTML = createMovieDetailTemplate(movie);
 	},
 };
-
+ 
 export default Detail;
