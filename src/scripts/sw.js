7656b5a33be0cf80fb6fbde9e19041de0ae092a4
dian-tri-budianto <dian.budianto@berca.co.id>
2021-04-20 06:20:10 +0700

    Implementation cache

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,13 +1,18 @@
+import CacheHelper from "./utils/cache-helper";
+import 'regenerator-runtime';
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
 	console.log('Installing service worker...!!!');
 
-	// TODO caching app shell resource
+	event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
 	console.log('Activating service worker...!!!');
 
-	// TODO delete old caches
+	event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
@@ -15,6 +20,6 @@ self.addEventListener('fetch', (event) => {
 
 	event.respondWith(fetch(event.request));
 
-	// TODO add/get fetch request to/from caches
+	event.respondWith(CacheHelper.revalidateCache(event.request));
 	
 });
